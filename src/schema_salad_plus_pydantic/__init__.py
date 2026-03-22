684e126994694d03a12c6777253712b4160979e9
John Chilton <jmchilton@gmail.com>
2026-03-21 23:40:19 -0400

    Starting work on 0.1.2

--- src/schema_salad_plus_pydantic/__init__.py
+++ src/schema_salad_plus_pydantic/__init__.py
@@ -1,6 +1,6 @@
 """Generate pydantic v2 BaseModel classes from schema-salad definitions."""
 
-__version__ = "0.1.1"
+__version__ = "0.1.2.dev0"
 
 PROJECT_NAME = "schema-salad-plus-pydantic"
 PROJECT_AUTHOR = "jmchilton"
